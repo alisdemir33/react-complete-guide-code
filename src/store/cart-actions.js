996074a1e875a3d8f81963084e7926def2b7a2c1
alisdemir33 <alisdemir33@gmail.com>
2021-09-10 07:20:22 +0300

    initiall

---
+++ src/store/cart-actions.js
@@ -0,0 +1,61 @@
+import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
+
+
+
+export const fetchCartData = () => {
+    return async (dispatch) => {
+
+        const fetchData = async () => {
+
+            const response = await fetch('https://react-http-asd-default-rtdb.europe-west1.firebasedatabase.app/cart.json')
+
+            if (!response.ok) {
+                throw new Error(' Fetch Cart Failed!')
+            }
+
+            const data = await response.json();
+            return data;
+        }
+
+        try {
+            const cartData = await fetchCartData()
+
+            dispatch(cartActions.replaceCart(
+                {
+                    items:cartData.items || [], 
+                    totalQuantity:cartData.totalQuantity
+                })
+                    )
+        } catch (error) {
+            dispatch(uiActions.showNotification({ status: 'error', title: 'Error!!', message: 'Error Ocurred' }));
+        }
+    }
+}
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({ status: 'PENDING', title: 'sending', message: 'Sending Cart' }));
+
+        const sendRequest = async () => {
+            const response = await fetch('https://react-http-asd-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+                {
+                    method: 'PUT', //olanın yerine koyar
+                    body: JSON.stringify({items:cart.items, totalQuantity:cart.totalQuantity})
+                })
+
+            if (!response.ok) {
+
+                throw new Error('Send Failed!')
+            }
+        }
+
+        try {
+            sendRequest(cart);
+            dispatch(uiActions.showNotification({ status: 'success', title: 'success', message: 'Cart Sent' }));
+        } catch (error) {
+            dispatch(uiActions.showNotification({ status: 'error', title: 'Error!!', message: 'Error Ocurred' }));
+        }
+
+    }
+}
